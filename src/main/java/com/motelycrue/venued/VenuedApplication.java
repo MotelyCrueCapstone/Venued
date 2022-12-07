cb2f3413fd99917cc37e25ede6ce20e712bcdba4
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-12-07 15:30:24 -0600

    autosave script

--- src/main/java/com/motelycrue/venued/VenuedApplication.java
+++ src/main/java/com/motelycrue/venued/VenuedApplication.java
@@ -1,12 +1,22 @@
 package com.motelycrue.venued;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class VenuedApplication {
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 
-    public static void main(String[] args) {
+//@SpringBootApplication
+public class VenuedApplication {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(VenuedApplication.class, args);
     }
 
--- src/main/java/com/motelycrue/venued/services/EventbriteApiService.java
+++ src/main/java/com/motelycrue/venued/services/EventbriteApiService.java
@@ -1,10 +1,33 @@
 package com.motelycrue.venued.services;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.squareup.okhttp.*;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
 
 @Service("eventbriteService")
 public class EventbriteApiService{
 
+    private static final String baseUrl = "https://www.eventbriteapi.com/v3";
+    private static final String test = "https://www.eventbriteapi.com/v3/users/me/?token=HVK7EH42PKKZTOPGDCE6";
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
+    //sending test requests to api service
+    public void setEndpoint(String endpoint, String json) throws IOException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(JSON, json);
+
+        Request request = new Request.Builder()
+                .url(test)
+                .build(); // defaults to GET
+        Response response = client.newCall(request).execute();
+    }
 }
