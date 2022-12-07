47e394492bc242aa83968655d7aa9a04960cb584
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-12-07 12:16:09 -0600

    services

--- src/main/java/com/motelycrue/venued/services/EventbriteApiService.java
+++ src/main/java/com/motelycrue/venued/services/EventbriteApiService.java
@@ -3,6 +3,8 @@ package com.motelycrue.venued.services;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class EventBriteApiService{
+@Service("eventbriteService")
+public class EventbriteApiService{
+
+
 }
