7b28acea282e67dec0e572d3805a354431d50463
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-12-08 09:55:36 -0600

    usercontroller

--- src/main/java/com/motelycrue/venued/users/UserController.java
+++ src/main/java/com/motelycrue/venued/users/UserController.java
@@ -4,13 +4,17 @@ import com.motelycrue.venued.badge.BadgeRepository;
 import com.motelycrue.venued.venues.VenueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository userDao;
 
     @Autowired
     private BadgeRepository BadgeDao;
@@ -18,4 +22,19 @@ public class UserController {
     @Autowired
     private VenueRepository VenueDao;
 
+    private PasswordEncoder passwordEncoder;
+
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model){
+        model.addAttribute("user", new User());
+        return "/registration";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userDao.save(user);
+        return "redirect:/posts/create";
+    }
 }
