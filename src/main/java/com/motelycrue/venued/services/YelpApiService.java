05ef7c01ed75cecd4c119009e9944f4dc8dddc98
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-12-10 23:03:42 -0600

    this is a tets

--- src/main/java/com/motelycrue/venued/services/YelpApiService.java
+++ src/main/java/com/motelycrue/venued/services/YelpApiService.java
@@ -35,17 +35,11 @@ public class YelpApiService {
 
 
     //sending test requests to api service
-<<<<<<< HEAD
     public static String execute(String query) throws IOException {
 
         OkHttpClient client = new OkHttpClient();
 
         //the object that turns POJOS into JSON objects
-=======
-
-
-    public String execute(String query) throws IOException {
->>>>>>> 530d2c93cb8558220f966741616d5c717ac66439
         ObjectMapper objectMapper = new ObjectMapper();
 
         //creating the api endpoint url
@@ -55,16 +49,10 @@ public class YelpApiService {
         //turns the url into a viable http requestt with the
         // Bearer token so we can receive the results of our query
         Request request = new Request.Builder()
-<<<<<<< HEAD
                 .url(requestUrl)
                 .addHeader("Authorization", "Bearer " + yelpApiKey)
-=======
-                .url("https://api.yelp.com/v3/businesses/search?location=san%20antonio%20&term=music&radius=2000&categories=clubs&sort_by=best_match&limit=20"
-                )
-                .addHeader("Authorization", "Bearer " + "ad95qSYlKKcn-LAdY3BL5y2d6wPKiG028RptoMyqv7IGbVG85KyJINy1MKCm6Zbl-IPxtuv6daqmH8zVF0LYoqGPR6yqMr9sdMuilmweDa4xjY66xfFVL1hb7BCSY3Yx")
                 .addHeader("accept", "application/json")
                 .addHeader("Content-Type", "application/json")
->>>>>>> 530d2c93cb8558220f966741616d5c717ac66439
                 .build();
 
 
@@ -78,30 +66,11 @@ public class YelpApiService {
         //trying to pull the business key from the json object returned fromm the initial api call
         //so i can iterate through them and add them to the allBusinessesJson with api calls to each Id.
 
-<<<<<<< HEAD
 //        if(objectNode.has("businesses")){
 //            return(objectNode.get("businesses").asText());
 //        }else{
 //            return(responseString);
 //        }
-=======
-            return objectNode.toString();
     }
 
-    public String execute(String query, String latitude, String longitude) throws IOException {
-        ObjectMapper objectMapper =  new ObjectMapper();
-        //creating the api endpoint url
-        String requestUrl = makeAutocompleteUrl(query, latitude, longitude);
-
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url(requestUrl)
-                .build();
-        Response response = client.newCall(request).execute();
-
-        //returns businesess and business ids near lat/long values
-        return response.body().string();
->>>>>>> 530d2c93cb8558220f966741616d5c717ac66439
-    }
 }
