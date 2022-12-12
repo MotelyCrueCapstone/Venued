34159361524d58a73bea5aa70c59140ba3a281cd
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-12-12 10:15:06 -0600

    autosave script

--- src/main/java/com/motelycrue/venued/yelp_api/YelpApiController.java
+++ src/main/java/com/motelycrue/venued/yelp_api/YelpApiController.java
@@ -3,18 +3,18 @@ package com.motelycrue.venued.yelp_api;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import retrofit2.http.Path;
 
 import java.io.IOException;
 
 @Controller
-@RequestMapping("/yelp")
 public class YelpApiController {
-
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public String yelp() throws IOException {
-        return YelpApiService.execute("clubs");
+    @GetMapping(value = "/event/{query}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public String yelp(@PathVariable String query) throws IOException {
+        return YelpApiService.execute(query);
     }
 }
