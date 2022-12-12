65dd66f89ff305eb9d0b91bbaca6a5867fb31881
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-12-12 16:20:21 -0600

    autosave script

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -1,3 +1,6 @@
 "use strict";
 
+fetch("http://localhost:8083/event/food?latitude=29.424122&longitude=-98.493629", {method: "GET"})
+     .then(response => response.json())
+     .then(json =>  console.log(json))
 
