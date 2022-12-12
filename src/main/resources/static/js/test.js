e8a6544483ee39c902bd1a543ace2dd02f6ac3f6
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-12-12 16:29:18 -0600

    working retrieving venues by user longitude latitude

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -1,6 +1,18 @@
 "use strict";
 
-fetch("http://localhost:8083/event/food?latitude=29.424122&longitude=-98.493629", {method: "GET"})
-     .then(response => response.json())
-     .then(json =>  console.log(json))
+$(function(){
+     //getting the users longitude/latitude to use for our servers api
+     navigator.geolocation.getCurrentPosition((position) => {
+          let lat = position.coords.latitude;
+          let long = position.coords.longitude;
+          
+          //gettign the venues according to the users location
+          fetch(`http://localhost:8083/event/food?latitude=${lat.toFixed(4)}&longitude=${long.toFixed(4)}`, {method: "GET"})
+          .then(response => response.json())
+          .then(json =>  console.log(json))
+     });
+})
+
+
+
 
