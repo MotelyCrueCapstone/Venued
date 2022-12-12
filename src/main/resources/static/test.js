04a0993c278d036996c4b6be68c94bf4dbcd8a64
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-12-12 11:16:25 -0600

    autosave script

--- src/main/resources/static/test.js
+++ src/main/resources/static/test.js
@@ -1,15 +1,1 @@
 "use strict";
-
-/*
- Creator: Deshawn Marquis, Williams,
- GitHub Profile: https://github.com/MarquisTheCoder,
- Date Created: 12/12/22,
- Time Created: 11:13 AM,
- File Name: test.js,
- File Description:
- */
-
-
-(function () {
-
-})();
