4d7b0a9818fa9353ccec7ccee6b0cbb28e105af3
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-12-12 12:32:52 -0600

    adding the support for longitude latitude parameters

--- src/main/java/com/motelycrue/venued/yelp_api/YelpApiController.java
+++ src/main/java/com/motelycrue/venued/yelp_api/YelpApiController.java
@@ -6,15 +6,29 @@ import org.springframework.web.bind.annotation.*;
 import retrofit2.http.Path;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Controller
 public class YelpApiController {
     @ResponseBody
-    @GetMapping(value = "/event/{query}/{long}/{latitude}",
+    @GetMapping(value = "/event/{query}",
             produces = MediaType.APPLICATION_JSON_VALUE)
+    // 	latitude: 29.50184
+    // -longitude: 98.49869
 
-    public String yelp(@PathVariable String query)
+    public String yelp(@PathVariable String query,
+                       @RequestParam Optional<String> latitude,
+                       @RequestParam Optional<String> longitude)
             throws IOException {
-        return YelpApiService.execute(query);
+
+
+        //making sure the longitude and latitude are both present in the
+        //parameters or the controller will return the default page for the
+        //venue or event
+        if(latitude.isPresent() && longitude.isPresent()){
+            return YelpApiService.execute(query, latitude.get(), longitude.get());
+        }else{
+            return YelpApiService.execute(query);
+        }
     }
 }
--- src/main/java/com/motelycrue/venued/yelp_api/YelpApiService.java
+++ src/main/java/com/motelycrue/venued/yelp_api/YelpApiService.java
@@ -15,13 +15,40 @@ public class YelpApiService {
 
     private static final String yelpBaseUrl = "https://api.yelp.com/v3/";
     private static final String businesses = "businesses/";
-    private static MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-
     //https://api.yelp.com/v3/businesses/search?location=san%20antonio%20&term=music&radius=2000&categories=clubs&sort_by=best_match&limit=20"
-    private static String makeAutocompleteUrl(String query, String location){
+    private static String makeAutoCompleteUrl(String query, String location){
         return String.format("%s%ssearch?location=%s&term=%s&radius=2000&categories=club&sort_by=best_match&limit=20",
                 yelpBaseUrl,businesses, location,query);
     }
+    private static String makeAutoCompleteUrl(String query, String latitude, String longitude){
+        return String.format("%s%ssearch?latitude=%s&longitude=%s&term=%s&radius=2000&categories=club&sort_by=best_match&limit=20",
+                yelpBaseUrl,businesses, latitude, longitude, query);
+    }
+    public static String execute(String query, String latitude, String longitude) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        //the object that turns POJOS into JSON objects
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        //creating the api endpoint url
+        String requestUrl = makeAutoCompleteUrl(query, latitude, longitude );
+
+        //turns the url into a viable http requestt with the
+        // Bearer token so we can receive the results of our query
+
+        Request request = new Request.Builder()
+                .url(requestUrl)
+                .addHeader("Authorization", "Bearer " + yelpApiKey)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String responseString  = response.body().string();
+        ObjectNode objectNode = objectMapper.readValue(responseString, ObjectNode.class);
+        return responseString;
+    }
+
+
     //sending test requests to api service
     public static String execute(String query) throws IOException {
 
@@ -31,10 +58,11 @@ public class YelpApiService {
         ObjectMapper objectMapper = new ObjectMapper();
 
         //creating the api endpoint url
-        String requestUrl = makeAutocompleteUrl(query, "San antonio");
+        String requestUrl = makeAutoCompleteUrl(query, "San antonio");
 
         //turns the url into a viable http requestt with the
         // Bearer token so we can receive the results of our query
+
         Request request = new Request.Builder()
                 .url(requestUrl)
                 .addHeader("Authorization", "Bearer " + yelpApiKey)
