7a4f97016bb595311867237032e600a850c01b56
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-12-14 14:33:28 -0600

    autosave script

--- src/main/java/com/motelycrue/venued/yelp_api/YelpApiController.java
+++ src/main/java/com/motelycrue/venued/yelp_api/YelpApiController.java
@@ -12,18 +12,22 @@ import java.util.Optional;
 public class YelpApiController {
     @ResponseBody
     @GetMapping(value = "/event/{query}", produces = MediaType.APPLICATION_JSON_VALUE)
+    // 	latitude: 29.50184
+    // longitude: -98.49869
+
     public String yelp(@PathVariable String query,
                        @RequestParam Optional<String> latitude,
                        @RequestParam Optional<String> longitude)
             throws IOException {
 
-            //making sure the longitude and latitude are both present in the
-            //parameters or the controller will return the default page for the
-            //venue or event
-
-//            if(latitude.isPresent() && longitude.isPresent()){
-        return YelpApiService.execute(query, latitude.get(), longitude.get());
-
+        //making sure the longitude and latitude are both present in the
+        //parameters or the controller will return the default page for the
+        //venue or event
+        if(latitude.isPresent() && longitude.isPresent()){
+            return YelpApiService.execute(query, latitude.get(), longitude.get());
+        }else{
+            //reminding frontend you need to add the longitude latitude to the request
+            return "{\"result\" : \"requires longitude/latitude combination\"}";
         }
+    }
 }
-
