0d65c8208552cbaa1e7a5b74d1656eeb8ce16584
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-12-14 11:09:28 -0600

    adding api key

--- src/main/java/com/motelycrue/venued/utility/APIAccess.java
+++ src/main/java/com/motelycrue/venued/utility/APIAccess.java
@@ -2,8 +2,11 @@ package com.motelycrue.venued.utility;
 
 public class APIAccess {
     private static String apiKey = "ad95qSYlKKcn-LAdY3BL5y2d6wPKiG028RptoMyqv7IGbVG85KyJINy1MKCm6Zbl-IPxtuv6daqmH8zVF0LYoqGPR6yqMr9sdMuilmweDa4xjY66xfFVL1hb7BCSY3Yx";
-
+    private static String predictHqApi = "Up7w8w55Ip2ehWeTYvrAOgrUVoSbIR3_RYm0xxG3hpivct0MOnVnkQ";
     public static String getApiKey() {
         return apiKey;
+    }
+    public static String getPredictHqApi(){
+        return predictHqApi;
     }
 }
