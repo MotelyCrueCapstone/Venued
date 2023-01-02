727ee7357e1db2d5afb98d6ae9c86a08c5c43d4f
francesperez <frances.perez141@gmail.com>
2023-01-02 17:52:19 -0600

    Responsiveness to homepage

--- src/main/resources/static/js/homepage/scrolling-reveal.js
+++ src/main/resources/static/js/homepage/scrolling-reveal.js
@@ -2,28 +2,22 @@
 $(function(){
      ScrollReveal().reveal('.reveal1', {
           origin: 'left',
-          delay: 500,
+          delay: 300,
           duration: 1600,
           distance: '1000px',
      })
-     
+
      ScrollReveal().reveal('.reveal2', {
           origin: 'right',
-          delay: 500,
+          delay: 300,
           duration: 1600,
           distance: '1000px',
      })
-     
-     ScrollReveal().reveal('.reveal3', {
-          origin: 'left',
-          delay: 500,
-          duration: 1600,
-          distance: '1000px',
-     })
-     
+
+
      ScrollReveal().reveal('.reveal', {
           origin: 'bottom',
-          delay: 500,
+          delay: 300,
           duration: 1600,
           distance: '1000px',
      })
