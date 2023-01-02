b741810b84a44a042a155ff5fe085867c6200a8b
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-01-02 11:16:30 -0600

    autosave script

---
+++ src/main/resources/static/js/homepage/results.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/*
+ Creator: Deshawn Marquis, Williams,
+ GitHub Profile: https://github.com/MarquisTheCoder,
+ Date Created: 1/2/23,
+ Time Created: 10:15 AM,
+ File Name: results.js,
+ File Description:
+ */
+
+
+(function () {
+    let input = $(".search_input")
+    let searchButton = $(".search_icon")
+    
+    function search(query){
+    
+    }
+})();
