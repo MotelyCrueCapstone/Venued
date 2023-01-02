61f7c5ac60c935e3c675605016e5c4af369c5653
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-01-02 10:04:52 -0600

    autosave script

--- src/main/java/com/motelycrue/venued/results/ResultsController.java
+++ src/main/java/com/motelycrue/venued/results/ResultsController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ResultsController {
     @GetMapping
     public String resultsPage(){
-        return "results";
+        return "results_page";
     }
 
 }
