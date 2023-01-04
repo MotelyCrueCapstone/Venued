84f4fec1d83c2f8b828b2341082aa606e133f75a
francesperez <frances.perez141@gmail.com>
2023-01-04 16:00:30 -0600

    dummy data

--- dummyData/questions.sql
+++
@@ -1,7 +0,0 @@
-USE venued_db;
-
-INSERT INTO questions(id, question, user_id, venue_id) VALUES
-    (1, "Does anyone know if this place has a ChickFilA?", 1, 2),
-    (2, "How much is the parking right in front of the stadium??", 4, 2),
-    (3, "Do they card/id ppl under 18 after 10PM?", 3, 1),
-    (4, "Do they check the inside of your bag when you enter the place?", 2, 3)
