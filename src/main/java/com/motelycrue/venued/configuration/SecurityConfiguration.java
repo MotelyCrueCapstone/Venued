8e948198fe6648f527d18733ac6dec7d01a7ed67
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-01-06 16:15:12 -0600

    autosave script

--- src/main/java/com/motelycrue/venued/configuration/SecurityConfiguration.java
+++ src/main/java/com/motelycrue/venued/configuration/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -23,17 +24,18 @@ public class SecurityConfiguration {
     // Bean to configure filter chain and set which pages must be authorized to view
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authConfig -> {
-            authConfig.anyRequest().permitAll();
+        http.authorizeHttpRequests((authorize)-> {
+            authorize.anyRequest().permitAll();
+//            authorize.requestMatchers("/","/home", "/venues/**").permitAll();
 //            authConfig.requestMatchers("/").permitAll().and(;
 //            authConfig.requestMatchers("/home").permitAll();
 //            authConfig.requestMatchers("/login").permitAll();
 //            authConfig.requestMatchers("/register").permitAll();
 //            authConfig.requestMatchers("/venues").authenticated();
 
-        })
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
+        });
+//                .formLogin(Customizer.withDefaults())
+//                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
