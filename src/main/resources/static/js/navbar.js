350bd5e54ea43bfd44325ff90ca06599855923cb
francesperez <frances.perez141@gmail.com>
2023-01-06 17:00:07 -0600

    autosave command

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -1,5 +1,3 @@
-
-
 $(function(){
     let toggleButton = $('.toggle-button');
     let navbarLinks = $('.nav-items');
@@ -11,5 +9,35 @@ $(function(){
         console.log("clicked");
     });
 });
+
+$(function() {
+    // Retrieve and parse the JSON file
+    $.getJSON('/json/cities.json', function(data) {
+        // Create the dropdown menu
+        const select = $("<select></select>");
+
+        // Add options to the dropdown menu
+        $.each(data, function(key, value) {
+            select.append($("<option></option>").attr("value", key).text(value));
+        });
+
+        // Add the dropdown menu to the container
+        $("#search_icon_container").append(select);
+    });
+
+    // When the user types in the search bar, update the dropdown menu
+    $("#search_location_input").on("keyup", function() {
+        // Get the search term the user entered
+        const searchTerm = $(this).val().toLowerCase();
+
+        // Show options in the dropdown menu that contain the search term
+        $("#search_icon_container select option").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(searchTerm) > -1);
+        });
+    });
+});
+
+
+
 
 
