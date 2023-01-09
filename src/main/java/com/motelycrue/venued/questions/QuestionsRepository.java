95565118e986d0a0c2ac85fd96e9eee7d855b252
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-01-09 12:23:59 -0600

    autosave script

--- src/main/java/com/motelycrue/venued/questions/QuestionsRepository.java
+++ src/main/java/com/motelycrue/venued/questions/QuestionsRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface QuestionsRepository extends JpaRepository<Questions, Long> {
 
     List<Questions> findByVenue(Venue venue);
