3fd3ff850991c9f33cae2db321cb4b34c073a4e7
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-01-11 16:19:21 -0600

    autosave script

--- src/main/resources/static/js/homepage/scrolling-reveal.js
+++ src/main/resources/static/js/homepage/scrolling-reveal.js
@@ -1,25 +1,27 @@
 "use strict";
+
 $(function(){
+     
      ScrollReveal().reveal('.reveal1', {
           origin: 'left',
           delay: 300,
-          duration: 1600,
-          distance: '1000px',
+          duration: 1000,
+          distance: '100px',
      })
 
      ScrollReveal().reveal('.reveal2', {
           origin: 'right',
           delay: 300,
-          duration: 1600,
-          distance: '1000px',
+          duration: 1000,
+          distance: '100px',
      })
 
 
      ScrollReveal().reveal('.reveal', {
           origin: 'left',
           delay: 300,
-          duration: 1600,
-          distance: '1000px',
+          duration: 1000,
+          distance: '100px',
      })
 })
 
