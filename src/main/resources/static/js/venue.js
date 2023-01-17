9f569b4d3c80440badd47f006b9fe79b7f20c762
francesperez <frances.perez141@gmail.com>
2023-01-17 12:07:31 -0600

    autosave command

--- src/main/resources/static/js/venue.js
+++ src/main/resources/static/js/venue.js
@@ -17,24 +17,44 @@ $(async function() {
     }
 
     function updateEvent() {
-        const title = events[eventIndex].title;
-        const description = events[eventIndex].description;
-        let date = new Date(events[eventIndex].start);
-        const eventDate =`${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`;
+        while (true) {
+            let date = new Date(events[eventIndex].start);
+            if (date > new Date()) {
+                const title = events[eventIndex].title;
+                const category = events[eventIndex].category;
+                const eventDate =`${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`;
 
-        $('.title-info').text(title);
-        $('.desc-info').text(description);
-        $('.event-date').text(eventDate);
-        eventIndex++;
-        if (eventIndex >= events.length) {
-            eventIndex = 0;
+                $('.title-info').text(title);
+                $('.desc-info').text(category);
+                $('.event-date').text(eventDate);
+
+                if (events[eventIndex].entities && events[eventIndex].entities[0] && events[eventIndex].entities[0].name) {
+                    const venue = events[eventIndex].entities[0].name;
+                    $('.event-venue').text(venue);
+                } else {
+                    $('.event-venue').text("");
+                }
+
+                eventIndex++;
+                if (eventIndex >= events.length) {
+                    eventIndex = 0;
+                }
+                break;
+            }
+            eventIndex++;
+            if (eventIndex >= events.length) {
+                eventIndex = 0;
+            }
         }
     }
+
+
+
 
     let mainSection = $('#main-section')
     let latitude = $(mainSection).data("venue-latitude");
     let longitude = $(mainSection).data("venue-longitude");
 
     grabEvents(latitude, longitude);
-    setInterval(updateEvent, 7000);
+    setInterval(updateEvent, 1000);
 });
