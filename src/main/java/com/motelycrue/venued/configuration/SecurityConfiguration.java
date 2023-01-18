bde61bfe7d802a59691cc946dce7e6f98e2b26a9
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-01-18 11:47:06 -0600

    autosave script

--- src/main/java/com/motelycrue/venued/configuration/SecurityConfiguration.java
+++ src/main/java/com/motelycrue/venued/configuration/SecurityConfiguration.java
@@ -1,40 +1,25 @@
 package com.motelycrue.venued.configuration;
 
-import com.motelycrue.venued.users.RefererRedirectionAuthenticationSuccessHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration{
     //tells the configuration file to add this to the spring
     //apps configuration
-
-
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
     @Bean
     SecurityFilterChain customSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
 
-                .antMatchers("/","/home", "/register").permitAll().and().authorizeHttpRequests()
+                .antMatchers("/","/home", "/register").permitAll()
                 .antMatchers("/profile").authenticated()
-                .and().formLogin().successHandler(new RefererRedirectionAuthenticationSuccessHandler()).loginPage("/login").defaultSuccessUrl("/home").permitAll()
+                .and().formLogin().loginPage("/login")
                 .and().logout()
                 .and().httpBasic();
 
