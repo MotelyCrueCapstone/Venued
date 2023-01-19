40b7af5312c8e4e32786e08c73cf9916ec918b9f
tesfalem tekie <teshanna72016@gmail.com>
2023-01-18 21:34:43 -0600

    venue modified

--- src/main/java/com/motelycrue/venued/questions/QuestionsController.java
+++ src/main/java/com/motelycrue/venued/questions/QuestionsController.java
@@ -69,6 +69,7 @@ public class QuestionsController {
 
         questionsDao.findById(Long.parseLong(questionId)).ifPresent(question1 -> {
             newAnswer.setQuestion(question1);
+
             question1.setAnswered(1);
 //            question1.setAnsweredQuestions(newAnswer);
             List <Answer> answerList = question1.getAnsweredQuestions();
